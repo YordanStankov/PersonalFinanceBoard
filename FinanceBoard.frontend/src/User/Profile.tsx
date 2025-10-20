c2bc25677538d53c98d182d5d1c87e333a6cb4be
dstankov2107 <yordanstankov2107@gmail.com>
2025-10-20 21:47:30 +0300

    data is displayed Categories display needs a little more work

--- FinanceBoard.frontend/src/User/Profile.tsx
+++ FinanceBoard.frontend/src/User/Profile.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import type { CategoryList } from '../Models/DTOs/Category/CategoryList';
 import type { TransactionList } from '../Models/DTOs/Transaction/TransactionList';
 import type { UserProfileDTO } from '../Models/DTOs/User/UserProfileDTO';
 import { jwtDecode } from 'jwt-decode';
 
-var profile : UserProfileDTO = { userName: ""};
+
 // var time : string = "";
 var categories : CategoryList[] = [];
 
@@ -15,15 +16,17 @@ var categories : CategoryList[] = [];
 //     time = formatedDateString;
 // }
 
- async function GetUserProfile( ){
+ async function GetUserProfile( ) : Promise<UserProfileDTO | void>{
+    var profile : UserProfileDTO = { userName: ""};
     var id : string = "";
-    var user = localStorage.getItem(`User`);
-    var jsonUser = JSON.parse(user ?? "user is null")
+
+    const user = localStorage.getItem(`User`);
+    const jsonUser = JSON.parse(user ?? "user is null");
     console.log(jsonUser);
     id = jsonUser.id;
 
-    var token = localStorage.getItem(`token`) ?? "jwtToken";
-    var credentials = jwtDecode(token)
+    const token = localStorage.getItem(`token`) ?? "jwtToken";
+    const credentials = jwtDecode(token)
     console.log(credentials);
     
 
@@ -50,45 +53,75 @@ var categories : CategoryList[] = [];
         console.log(response.status)
         console.log(response)
     }
-
+    return await profile;
 }
 
-// function DisplayCategoryTransactions(Transactions : TransactionList[]){
-//     for(var i = 0; i < Transactions.length; i++){
-//         return <>
-//          <h2>Amount: {Transactions[i].Amount}</h2>
-//          <h2>Category: {Transactions[i].CategoryName}</h2>
-//          <h2>Description: {Transactions[i].Description}</h2>
-//          <h2>Time of transaction: {time}</h2>
-//             <h2>Id: {Transactions[i].Guid}</h2>
-//         </>
-//     }
-// }
-// function DisplayUserCategories(categories: CategoryList[]){
-//     for(var i = 0; i < categories.length; i++ ){
-//         return <>
-//         <div>
-//             <h2>{categories[i].Name} </h2>
-//                 <div className="TransactionsContainer">
-//                     {DisplayCategoryTransactions(categories[i].TransactionList)}
-//                 </div>
-//         </div>
-//     </>
-//     }
-   
-// }
+function DisplayCategoryTransactions(Transactions : TransactionList[]){
+    if(!Transactions || Transactions.length === 0){
+        return <h2>No transactions for this user.</h2>;
+    }
+    else{
+            for(var i = 0; i < Transactions.length; i++){
+            return <>
+            <h2>Amount: {Transactions[i].Amount}</h2>
+            <h2>Category: {Transactions[i].CategoryName}</h2>
+            <h2>Description: {Transactions[i].Description}</h2>
+            <h2>Time of transaction: dd..yyyy</h2>
+            <h2>Id: {Transactions[i].Guid}</h2>
+        </>
+        }
+    }
+    
+}
+function DisplayUserCategories(categories: CategoryList[]){
+    if(categories.length === 0 || !categories){
+        return <h2>No categories for this user.</h2>;
+    }
+    
+    else{
+      
+        for(var i = 0; i < categories.length; i++ ){
+                return <>
+                <h2>Categories: </h2>
+            <div>
+                 <h2>{categories[i].Name} </h2>
+                    <div className="TransactionsContainer">
+                    {DisplayCategoryTransactions(categories[i].TransactionList)}
+                    </div>
+            </div>
+            </>
+        }
+    }
+}
+
+function SetProfile(data : React.Dispatch<React.SetStateAction<UserProfileDTO>>) {
+    React.useEffect(() => {
+        async function fetchProfile() {
+            const profileData =  await GetUserProfile();
+            console.log("Fetched profile data:", profileData);
+            if(profileData){
+                data(profileData);
+            }
+        }
+        fetchProfile();
+    }, []
+    );
+};
+
 
 function Profile() {
-    GetUserProfile()
+    const [profile, setProfile] =  React.useState<UserProfileDTO>({userName: ""});
+    SetProfile(setProfile);
     return (
         <>
+        
         <div className="profile-container">
             <h2>UserName : {profile.userName}</h2>
             <h2>Montlhy spending: {profile.MonthtlySpending}</h2>
             <h2>Monthly income: {profile.MonthlyIncome}</h2>
             <h2>Avergae daily spending: {profile.AverageDailySpending}</h2>
         </div>
-        {/* {DisplayUserCategories(profile.Categories ?? categories)}  */}
+        {DisplayUserCategories(profile.Categories ?? categories)} 
 </>
     );
 }
